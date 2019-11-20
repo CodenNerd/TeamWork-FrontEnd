3406adcd67ca458a9b09fb345d319fc56e72e9fa
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-20 07:18:09 +0100

    create signin button for index page

---
+++ teamwork/src/components/LoginButton.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+    
+    render() { 
+        return ( 
+            <button class='signin-button'>
+                Sign In
+            </button>
+         );
+    }
+}
+ 
+export default Index;
--- teamwork/src/components/pages/index.jsx
+++ teamwork/src/components/pages/index.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Logo from '../teamwork-logo-grey';
+import HeroPhoneChat from './../images/hero-phone-chat.gif';
+import LoginButton from '../LoginButton';
 class Index extends Component {
-    
-    render() { 
-        return ( 
-            <Logo/>
-         );
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <Logo />
+                    <aside class='hero-aside'>
+                        <LoginButton />
+
+                    </aside>
+                    <aside>
+
+                        <img class='hero-phone-chat' src={HeroPhoneChat} alt="Hero Phone Chat" />
+                    </aside>
+
+                </div>
+            </React.Fragment>
+        );
     }
 }
- 
+
 export default Index;
