8f298dae7e5b6fcc1a98ae1d98444c41e825004e
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-20 06:23:47 +0100

    create logo for index page

---
+++ teamwork/src/components/pages/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Logo from '../teamwork-logo-grey';
+class Index extends Component {
+    
+    render() { 
+        return ( 
+            <Logo/>
+         );
+    }
+}
+ 
+export default Index;
---
+++ teamwork/src/components/teamwork-logo-grey.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+    
+    render() { 
+        return ( 
+            <h1 class="logo-grey">TeamWork</h1>
+         );
+    }
+}
+ 
+export default Index;
