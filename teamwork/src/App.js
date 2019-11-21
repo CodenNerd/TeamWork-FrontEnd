b16ff7724c54fb1ce805b114cf3eebbb12553105
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-21 14:35:09 +0100

    refactor - separate css for index page into a new file

--- teamwork/src/App.js
+++ teamwork/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Index from './components/pages/index';
 import { Switch, Route } from 'react-router-dom';
 import SignIn from './components/pages/signIn';
+// import PageNotFound from './components/pages/PageNotFound'
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Index} />
       <Route exact path="/signin" component={SignIn} />
+      {/* <Route component={PageNotFound} /> */}
       
     </Switch>
   );
