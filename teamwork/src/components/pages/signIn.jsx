14ebf2c12af755167457e0451707eb958f6c032d
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-21 18:18:32 +0100

    add undraw image to sign in page

--- teamwork/src/components/pages/signIn.jsx
+++ teamwork/src/components/pages/signIn.jsx
@@ -3,20 +3,25 @@ import RandomizeBoxes from './../RandomizeBoxes';
 import Logo from './../teamwork-logo-grey';
 import './../css/signin.css';
 import SignInForm from './../SignInForm'
+import SignInUndraw from './../images/signin-undraw.png'
 
 class SignIn extends Component {
 
     render() {
         return (
             <div>
-                <Logo/>
+                <Logo />
                 <aside className="hero-aside">
                     <RandomizeBoxes />
 
                     <h1 className="signin-text">Sign In</h1>
 
-                    <SignInForm/>
+                    <SignInForm />
                 </aside>
+                <aside className="phone-aside">
+                        <img className='hero-phone-chat' src={SignInUndraw} alt="Hero Phone Chat" />
+                        <footer>Teamwork &copy; 2019</footer>
+                    </aside>
             </div>
         );
     }
