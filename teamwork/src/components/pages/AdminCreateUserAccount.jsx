19cc1ef616137f642f8ee738acea3455a26c255c
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-23 23:43:44 +0100

    make admin dashboard card responsive across devices

--- teamwork/src/components/pages/AdminCreateUserAccount.jsx
+++ teamwork/src/components/pages/AdminCreateUserAccount.jsx
@@ -16,17 +16,19 @@ class SignIn extends Component {
                     <RandomizeBoxes />
                     
                     <div className="icons-div">
-                    
+                        <i className="far fa-user"></i>
+                        <i className="fas fa-user-plus icon-active"></i>
+                        <i className="far fa-flag"></i>
+                        
                     </div>
                     <div className="issues-div">
-                    
+                        <h3>Create User</h3>
                     </div>
                     <div className="activity-div">
                     
                     </div>
                 </div>
-               
-            </div>
+               </div>
         );
     }
 }
