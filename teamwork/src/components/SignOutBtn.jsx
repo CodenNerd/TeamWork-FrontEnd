4ff44b1f022ca68d5db457d80995c92934aa3b98
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-27 23:26:55 +0100

    style navbar and sign out

--- teamwork/src/components/SignOutBtn.jsx
+++ teamwork/src/components/SignOutBtn.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import userAuth from './../Controllers/Auth';
+import {Redirect} from 'react-router-dom';
 
 class SignOutBtn extends Component {
+   constructor(props){
+       super(props);
+       this.state = {
+           redirect: false
+       }
+   }
     
     handleClick = () =>{
         userAuth('destroy');
+        this.setState({redirect: true})
     }
     render() { 
-        return ( <button onClick={this.handleClick} className="so-btn">Sign Out</button> );
+        return ( 
+            <React.Fragment>
+        {this.state.redirect && <Redirect to='/signin' />}
+        <button onClick={this.handleClick} className="so-btn">Sign Out</button>
+        </React.Fragment>
+     );
     }
 }
  
