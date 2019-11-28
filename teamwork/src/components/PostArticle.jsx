a9fde09de19fda435a256a796694422646ae3787
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-28 11:11:27 +0100

    complete post article styling

--- teamwork/src/components/PostArticle.jsx
+++ teamwork/src/components/PostArticle.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
 class PostArticle extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.tag = [
+            'Adventure',
+            'Art',
+            'Baby',
+            'Beauty',
+            'Business',
+            'Craft',
+            'Decorating',
+            'Design',
+            'DIY',
+            'Education',
+            'Entertainment',
+            'Fashion',
+            'Film',
+            'Fitness',
+            'Food',
+            'Gaming',
+            'Health',
+            'HomeDecor',
+            'Humor',
+            'Lifestyle',
+            'Makeup',
+            'Marketing',
+            'MensFashion',
+            'Mom',
+            'MoneySaving',
+            'Music',
+            'Outdoor',
+            'Parenting',
+            'PersonalFinance',
+            'Pet',
+            'Photography',
+            'Political',
+            'Relationships',
+            'SelfHelp',
+            'Sewing',
+            'Sports',
+            'Tech',
+            'Travel',
+            'Wedding',
+            'IMPORTANT!',
+            'OFFICIAL!'
+        ]
+        this.state = {
+
+        }
+    }
     render() {
         return (
             <div className="post-article">
-                <div className="article-title"> <input type="text" /><span>Title</span> </div><div className="category"> <span className="tag"></span></div>
+                <div className="article-title"> <input type="text" /><span>Title</span> </div><div className="category"> {this.tag.map((tagText, i)=><span key={i} onClick={(tagText)=>this.props.onTag(tagText)} className="tag">{tagText}</span>)}</div>
                 <div className="article-body"> <textarea name="" id="" rows="5"></textarea> </div>
 
             </div>
--- teamwork/src/components/PostComponent.jsx
+++ teamwork/src/components/PostComponent.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import PostArticle from './PostArticle';
+import PostGif from './PostGif';
 
 class PostComponent extends Component {
-    state = {  }
-    render() { 
-        return ( 
-        <div className="post-component">
-            <div className="post-text">
-                POST:
-            </div>
-            <div className="buttons-tab">
-                <button className="article-btn">Article</button>
-                <button className="gif-btn">Gif</button>
+    constructor(props){
+        super(props);
 
-                <button className="publish-btn">Publish</button>
+        this.state = {
+            active: 'article'
+        }
+    }
+
+    render() {
+        return (
+            <div className="post-component">
+                <div className="post-text">
+                    POST:
             </div>
-            <div className="fields-div">
-                <PostArticle />
+                <div className="buttons-tab">
+                    <button onClick={()=>this.setState({active: 'article'})} id={this.state.active === 'article' ? 'post-active' : null} className="article-btn">Article</button>
+                    <button onClick={()=>this.setState({active: 'gif'})} id={this.state.active === 'gif' ? 'post-active' : null} className="gif-btn">Gif</button>
+
+                    <button className="publish-btn">Publish</button>
+                </div>
+                <div className="fields-div">
+                    {this.state.active==='article' && <PostArticle />}
+                    {this.state.active==='gif' && <PostGif />}
+                </div>
             </div>
-        </div>
-     );
+        );
     }
 }
- 
+
 export default PostComponent;
